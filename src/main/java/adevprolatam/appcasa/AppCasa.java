5c29fa92b8c0e0208b037d2df5fa5d741638d983
Adrian <adevprolatam@gmail.com>
2023-12-16 23:59:34 -0500

    First_Commit

---
+++ src/main/java/adevprolatam/appcasa/AppCasa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package adevprolatam.appcasa;
+
+/**
+ *
+ * @author dell
+ */
+public class AppCasa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
