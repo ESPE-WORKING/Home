21f4854f2388b5b47beee49ba0add8185b7083cb
Adrian <adevprolatam@gmail.com>
2023-12-17 00:14:56 -0500

    Add_Class_Persona_Casa

---
+++ src/main/java/adevprolatam/appcasa/Casa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adevprolatam.appcasa;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ *
+ * @author dell
+ */
+public class Casa {
+     private String direccion;
+     private List<Persona> habitantes;
+
+     public Casa(String direccion) {
+         this.direccion = direccion;
+         this.habitantes = new ArrayList<>();
+     }
+
+    public void agregarHabitante(Persona persona) {
+           habitantes.add(persona);
+    }
+    
+
+}
+
---
+++ src/main/java/adevprolatam/appcasa/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adevprolatam.appcasa;
+
+/**
+ *
+ * @author dell
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
